49199475edc0c3493256d97f068de8f3825db0c9
hakanyarman <104122864+hakanyarman@users.noreply.github.com>
2023-09-21 23:37:20 +0300

    useState practice

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import { useState } from "react";
 import Ahmet from "./components/Ahmet";
 import Person from "./components/Person";
+import HideShow from "./components/HideShow";
 import { Button, Alert } from 'react-bootstrap';
 
 
 function App() {
+  const [count, setCount] = useState(0)
+  // setCountu direkt buttonOnClick'de fonksiyon olarak çağıramayız başka bir fonksiyonun içinde çağırmamız gerek.
+
+  const [renk, setRenk] = useState('green')
+
+  const [status, setStatus] = useState(false);
+
+  const sayacArt = () => setCount(count + 1);
+  const sayacAzalt = () => {
+    if (count > 0) setCount(count - 1);
+  }
+  const changeColor = () => {
+    if (renk === 'green') {
+      setRenk('red');
+    } else {
+      setRenk('green')
+    }
+  }
+
+  const gizleGoster = () => {
+    setStatus(!status);
+  }
   const variable = 'Mehmet';
   let propsDeneme = "Hakan";
   const clickFunc = () => {
@@ -38,6 +62,19 @@ function App() {
       </>
       {/* map ile çalışırken her bir element için unique bir key verilmeli. */}
 
+      <Button onClick={sayacArt} >Arttır</Button>
+      <Button onClick={sayacAzalt} className="mx-3">Azalt</Button>
+
+      <div>{count}</div>
+
+      <div style={{ color: renk }} >Styled div</div>
+      <Button onClick={changeColor}>Change Color of div</Button>
+      <br></br>
+      <br></br>
+
+
+      <Button onClick={gizleGoster} >Show/Hide Component</Button>
+      {status ? <HideShow /> : null}
     </div >
     // önce parent componentde propps gir child componente sonra child componentde tanımla.
 
